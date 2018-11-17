68e49198d95bc27977fae4489259d32ad3be99e8
คุณนุ้ย <44543676+nongKoh@users.noreply.github.com>
2018-11-17 17:02:46 +0700

    Update node.java

--- node.java
+++ node.java
@@ -1,75 +1,43 @@
+import java.util.ArrayList;
 public class node{
-  //ตัวแปร
-  //ชื่อ
-  private String name;
-  //เพศ
-  private String sex;
-  //อายุ
-  private int age;
-  //น้ำหนัก
-  private double weight;
-  //ส่วนสู.
-  private double height;
-  //BMI
-  private double bmi=0;
-  //constructor
-  public node(){
-    this.name ="";
-    this.sex ="";
-    this.age =0;
-    this.weight=0;
-    this.height=0;
+  public String name;
+  public boolean x=false;
+  public ArrayList<node> mom = new ArrayList<node>();
+  public ArrayList<node> link = new ArrayList<node>();
+  public node(String t){
+    this.name = t;
   }
-  //ใส่ชื่อ
-  public void set_name(String name){
-  this.name+=name;
+  public void add_bool(boolean x){
+  this.x=x;
   }
-  //ใส่เพศ
-  public void set_sex(String sex){
-  this.sex=sex;
+  public void add_node(node next){
+    this.link.add(next);
   }
-  //ใส่อายุ
-  public void set_age(int age){
-  this.age=age;
+  public void add_mom(node next){
+    this.mom.add(next);
   }
-  //ใส่น้ำหนัก
-  public void set_weight(double weight){
-  this.weight=weight;
+  public boolean get_bool(){
+  return x;
   }
-  //ใส่ส่วนสูง
-  public void set_height(double height){
-  this.height=height;
-  }
-  //แสดงชื่อ
-  public String get_name(){
-  return this.name;
-  }
-  //แสดงเพศ
-  public String get_sex(){
-  return this.sex;
-  }
-  //แสดงอายุ
-  public int get_age(){
-  return this.age;
-  }
-  //แสดงน้ำหนัก
-  public double get_weight(){
-  return this.weight;
-  }
-  //แสดงส่วนสูง
-  public double get_height(){
-  return this.height;
-  }
-  //คำนวณBMI
-  public double get_bmi()throws ArithmeticException{
-  //ดักerror
-    if(height==0){
-    return this.bmi;
+  public boolean get_mom_and_bool(){
+    int cout=0;
+    for(int i =0;i<mom.size();i++){
+      if(mom.get(i).get_bool()){
+      cout++;
+      }
     }
-    else{
-  double newheight=height/100;
-  this.bmi=(weight/(newheight*newheight));
-  return this.bmi;
+    return cout==mom.size();
   }
+  public boolean get_mom_or_bool(){
+    int cout=0;
+    for(int i =0;i<mom.size();i++){
+      if(mom.get(i).get_bool()){
+      cout++;
+      }
+    }
+    return cout>=1;
+  }
+  public String get_node(){
+    return this.name;
   }
 }
